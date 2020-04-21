0cf0cd80fe618c3e9984ea5187746d28bdcc47f4
miggaz elquez <miguel.gazquez82@gmail.com>
2020-04-21 21:04:41 +0200

    change extr en un ensemble

--- gauss2d.py
+++ gauss2d.py
@@ -66,9 +66,9 @@ nby = int((ymax-ymin)/pas)
 tableau = [[ 0.0 ]*nbx for j in range(nby)]
 
 
-# Mise en place de listes contenant les extrema futurs pour x et y
-x_extr = []
-y_extr = []
+# Mise en place de l'ensemble contenant les extrema futurs pour x et y
+
+extr = {*()}
 
 
 # Mise en place des valeurs ainsi que des listes necessaires a la creation de multiples gaussienne 2-dimensionelles
@@ -93,12 +93,10 @@ for k in range(nb_gaussiennes):
             tableau[i][j] += gaussienne_2d( xmin+i*pas, ymin + j*pas, mx[k], my[k], sx[k], sy[k] )
 
 
-# Ici, on ajoute aux listes des extrema en x et y des extrema generes a partir du tableau
+# Ici, on ajoute à l'ensemble des extrema en x et y des extrema generes a partir du tableau
 #ex,ey = find_extremum(tableau, 0, nbx, 0, nby, 4, 4)
 ex,ey = find_extremum2(tableau,pas,0,0);
-x_extr.append(ex)
-y_extr.append(ey)
-
+extr.add((ex,ey))
 
 
 
@@ -113,7 +111,8 @@ plt.imshow(tableau, extent=[xmin, xmax, ymin, ymax], origin="lower", cmap='Blues
 plt.colorbar()
 
 # Affichage des extrema
-plt.plot(x_extr, y_extr,'ro', label='extrema')
+for x_extr, y_extr in extr:
+    plt.plot(x_extr, y_extr,'ro', label='extrema')
 
 # Affichage des bassins d'attraction
 #plt.imshow(tableau, extent=[xmin, xmax, ymin, ymax], origin='lower', cmap='RdGy', alpha=1.5)
